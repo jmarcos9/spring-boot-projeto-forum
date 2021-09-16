bc14730b38c37a9efc9f77fe44f235eec423e2e8
jmarcos9 <66381477+jmarcos9@users.noreply.github.com>
2021-09-16 16:59:49 -0300

    alter class
    add dependencia devtools para carregaento automatico do servidor
    retirado o responsebody e add @RestController

--- src/main/java/br/com/projeto/forum/controller/TopicosController.java
+++ src/main/java/br/com/projeto/forum/controller/TopicosController.java
@@ -6,20 +6,27 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import br.com.projeto.forum.controller.dto.TopicoDto;
 import br.com.projeto.forum.modelo.Curso;
 import br.com.projeto.forum.modelo.Topico;
 
-@Controller
+@RestController
 public class TopicosController {
-	
+
 	@RequestMapping("/topicos")
-	@ResponseBody
-	public List<Topico> lista() {
-		Topico topico = new Topico("Duvida", "Duvida Spring", new Curso("Spring", "Programação"));
-		
-		return Arrays.asList(topico, topico, topico);
+	public List<TopicoDto> lista() {
+		Topico topico = new Topico("Dúvida", "Dúvida Spring", new Curso("Spring", "Programação"));
+
+		return TopicoDto.converter(Arrays.asList(topico, topico, topico));
 	}
-	
 
 }
+
+/*
+ * echo "# spring-boot-project-forum" >> README.md git init git add README.md
+ * git commit -m "first commit" git branch -M main git remote add origin
+ * https://github.com/jmarcos9/spring-boot-project-forum.git git push -u origin
+ * main
+ */
